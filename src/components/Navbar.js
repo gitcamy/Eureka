60dbe81152ff1980c85520c3b706e91f749c4c9b
Camy <Camy@Camerons-MacBook-Pro.local>
2025-02-11 10:17:42 +0100

    Add disappearing navbar and fix button text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,42 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import { useEffect, useState, useCallback } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const handleScroll = useCallback(() => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY > lastScrollY) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+      setLastScrollY(window.scrollY);
+    }
+  }, [lastScrollY]); // Add lastScrollY as a dependency
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]); // Include handleScroll in the dependency array
+
+
   function toggleMenu() {
     const navLinks = document.querySelector(".nav-links");
     const hamburger = document.querySelector(".hamburger");
 
     navLinks.classList.toggle("active");
-    hamburger.classList.toggle("active"); // Toggle active class on hamburger
+    hamburger.classList.toggle("active");
   }
 
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${isVisible ? "visible" : "hidden"}`}>
       <div className="nav-brand">
         <Link to="/">
           <h2>Eureka</h2>
